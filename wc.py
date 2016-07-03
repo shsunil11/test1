1e59bc1a946578db087a0a19f3235051e33c7044
shsunil11 <sunil.shevale@gmail.com>
2016-07-03 12:36:45 -0400

    Create wc.py

---
+++ wc.py
@@ -0,0 +1,15 @@
+from pyspark import SparkConf, SparkContext
+import sys
+
+conf = SparkConf()
+
+sc = SparkContext(conf = conf)
+
+lines = sc.textFile(sys.argv[1])
+
+words = lines.flatMap(lambda line:  line.split(" ")).map(lambda word: (word,1)).reduceByKey(lambda x, y: x+y)
+
+for line in words.take(10):
+   print line
+
+sc.stop()
