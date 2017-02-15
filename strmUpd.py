e296baa3d545436ee7c2ff530f85dc0ff636c916
shsunil11 <sunil.shevale@gmail.com>
2017-02-14 21:02:19 -0500

    Update strmUpd.py

--- strmUpd.py
+++ strmUpd.py
@@ -3,24 +3,27 @@ import sys
 from pyspark import SparkConf,SparkContext
 from pyspark.streaming import StreamingContext
 
-conf = SparkConf().setAppName("Python Streaming")
+conf = SparkConf().setAppName("Streaming UpdateByKey")
 
 sc = SparkContext(conf = conf)
-sc.setLogLevel("WARN")
+#sc.setLogLevel("WARN")
 
+# sys.argv[1] is the first command line parameter which is batchInterval
 batchInterval = int(sys.argv[1])
 
 ssc = StreamingContext(sc, batchInterval)
 
-lines = ssc.socketTextStream("lbdp167a", 7777)
+lines = ssc.socketTextStream("localhost", 7777)
 
-ssc.checkpoint("/user/xx62883/streamcp")
+ssc.checkpoint("/user/cloudera/streamcp")
 users = lines.flatMap(lambda x: x.split(" "))
 
 userPairs = users.map(lambda x: (x,1))
 
 userPairs.pprint()
 
+# In the updateFunc, values is the values of a key in this batch and runningCount is the current state of the key
+# The current state of the key is maintained as the running count of the key
 def updateFunc(values, runningCount):
    if runningCount is None:
       runningCount = 0
@@ -28,8 +31,11 @@ def updateFunc(values, runningCount):
    return sum(values) + runningCount
    userState = userPairs.updateStateByKey(updateFunc)
 
-userState.saveAsTextFiles("/user/xx62883/streamOut","txt")
+# Save each batch to HDFS (folder will be created if it does not exist
+# "txt" is the suffix of the folder
+userState.saveAsTextFiles("/user/cloudera/streamOut","txt")
 
+# for each rdd print the top 20 elements in descendin order of the value (value = count in this case)
 def winRDD(rdd):
    print "Printing foreach RDD"
    for lines in rdd.takeOrdered(20, lambda (x,y): (-y,x)):
